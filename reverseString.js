e9ce2f112fba9e5fd620906e69b64a5021917f00
vaibhav2002desale <vaibhavdesale002@gmail.com>
2024-07-24 11:41:12 +0530

    programs

---
+++ reverseString.js
@@ -0,0 +1,22 @@
+function reverseString (str){
+
+
+    let String = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        String += str[i];
+    }
+    return String;
+}
+//jhgf
+
+
+let string = "Vaibhav";
+
+let result = reverseString(string);
+console.log(result);
+
+
+
+
+
+
